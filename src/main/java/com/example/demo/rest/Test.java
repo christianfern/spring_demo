ff04a823d8c51064fe6d0eb7dd6330f2852bd795
christian <51457144+christianfern@users.noreply.github.com>
2021-02-17 00:09:16 -0600

    - Implemented bootstrap - Added a few simple utility pages: word-counter, calculator - Refactored rest controllers into their own classes based on utility

---
+++ src/main/java/com/example/demo/rest/Test.java
@@ -0,0 +1,55 @@
+package com.example.demo.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Test")
+public class Test {
+
+    final String ADD = "+";
+    final String SUBTRACT = "-";
+    final String MULTIPLY = "*";
+    final String DIVIDE = "/";
+
+    @GetMapping("/hello")
+    public String sayHello(@RequestParam(value="myName", defaultValue="World") String name){
+        return String.format("Hello %s!", name);
+    }
+
+    @GetMapping("/calculator")
+    public String calculate(@RequestParam(value="x", defaultValue="0") int x,
+                           @RequestParam(value="y", defaultValue = "0") int y,
+                            @RequestParam(value="operator", defaultValue = "ADD") String operator){
+        int result = 0;
+
+        switch(operator){
+            case ADD:
+                result = x + y;
+                break;
+            case SUBTRACT:
+                result = x - y;
+                break;
+            case MULTIPLY:
+                result = x * y;
+                break;
+            case DIVIDE:
+                if(y == 0)
+                    return "You can't divide by zero!";
+                result = x / y;
+                break;
+        }
+
+        return String.format("%d %s %d = %d", x, operator, y, result);
+    }
+
+    @GetMapping("/wordCounter")
+    public String wordCounter(@RequestParam(value="sentence", defaultValue="") String sentence){
+        String[] words = sentence.split(" ");
+        String feedback = words.length > 1 ? "%d words" : "%d word";
+        return String.format(feedback, words.length);
+    }
+
+}
