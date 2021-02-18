7a27cc7cf6137a5e95fdbd589bc46a503911ccdb
christian <51457144+christianfern@users.noreply.github.com>
2021-02-17 23:51:18 -0600

    New POJO - Person New dummy services - generatePerson: create a Person object and return
    Other
    - Fix the name of the generateName rest service
    - Create a rest service that will create a list of Person objects using the new generatePerson service

--- src/main/java/com/example/demo/service/NoDBService.java
+++ src/main/java/com/example/demo/service/NoDBService.java
@@ -1,23 +1,33 @@
 package com.example.demo.service;
 
+import com.example.demo.pojo.Person;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
 import java.util.Random;
 
+@RequiredArgsConstructor
 @Component
 public class NoDBService {
 
-    final String[] firstNames = {"James", "Matthew", "Jose", "Sue", "Mary", "Emily"};
-    final String[] lastNames = {"Smith", "Lopez", "Wang", "Brown", "Jackson", "Connors"};
+    private final Random ran = new Random();
 
-    public String generateName(){
-        Random ran = new Random();
-        String firstName = firstNames[ran.nextInt(firstNames.length)];
-        String lastName = lastNames[ran.nextInt(lastNames.length)];
-        return firstName + " " + lastName;
+    private final String[] firstNames = {"James", "Matthew", "Jose", "Sue", "Mary", "Emily"};
+    private final String[] lastNames = {"Smith", "Lopez", "Wang", "Brown", "Jackson", "Connors"};
+
+    private String generateFirstName(){
+        return firstNames[ran.nextInt(firstNames.length)];
     }
 
+    private String generateLastName(){
+        return lastNames[ran.nextInt(lastNames.length)];
+    }
 
+    public String generateName(){
+        return String.format("%s %s", generateFirstName(), generateLastName());
+    }
 
+    public Person generatePerson(){
+        return new Person(generateFirstName(), generateLastName(), ran.nextInt(90 - 10) + 10);
+    }
 
 }
